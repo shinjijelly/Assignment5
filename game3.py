82c006e4644050840ef3a69180a10e28a991ab46
weeff <jhong1212@naver.com>
2024-05-31 12:32:30 +0900

    3. 블록 움직임 구현

--- game3.py
+++ game3.py
@@ -20,13 +20,19 @@ color = {0: (180, 180, 180),
 
 # 보드 판의 숫자
 board_values = [[0 for i in range(4)] for i in range(4)]
-# board_values = [ [2,4,8,16] , [32,64,128,256], [512,1024,2048,0],[0,0,0,0]]
+# board_values = [
+#     [0,0,0,2] ,
+#     [0,0,2,2] ,
+#     [0,2,0,0 ] ,
+#     [2,2,2,2]]
 
 game_over = False
 
+
 def game3_page(screen, font, WHITE, BLACK):
     global board_values
-    start = True
+    create = True
+    init_count = 0
     while True:
         screen.fill(color['bg'])
 
@@ -35,10 +41,10 @@ def game3_page(screen, font, WHITE, BLACK):
         draw_board(screen)
         draw_block(screen)
 
-        if (start):  # 처음 시작 시 블록 2개 생성
+        if (create or init_count < 2):  # 처음 시작 시 블록 2개 생성
             board_values, game_over = create_block(board_values)
-            board_values, game_over = create_block(board_values)
-            start = False
+            create = False
+            init_count += 1
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -47,10 +53,19 @@ def game3_page(screen, font, WHITE, BLACK):
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if home_button.collidepoint(event.pos):
                     return
-            elif event.type == pygame.KEYDOWN:
-                pass
             elif event.type == pygame.KEYUP:
-                pass
+                if event.key == pygame.K_UP:
+                    board_values = move('UP', board_values)
+                    create = True
+                elif event.key == pygame.K_DOWN:
+                    board_values = move('DOWN', board_values)
+                    create = True
+                elif event.key == pygame.K_RIGHT:
+                    board_values = move('RIGHT', board_values)
+                    create = True
+                elif event.key == pygame.K_LEFT:
+                    board_values = move('LEFT', board_values)
+                    create = True
 
         pygame.display.flip()
 
@@ -93,8 +108,91 @@ def create_block(board):
             board[y][x] = 4
         else:
             board[y][x] = 2
-
+    if full:
+        print("꽉 참")
     return board, full
+
+
+# 블록을 움직임
+def move(direction, board):
+    merged = [[False for i in range(4)] for i in range(4)]
+    if direction == 'UP':
+        for i in range(1, 4):
+            for j in range(4):
+                shift = 0
+                for k in range(i):
+                    if board[k][j] == 0:  # 위로 움직일 수 있는 공간이 있을 때
+                        shift += 1
+
+                if shift > 0:
+                    board[i - shift][j] = board[i][j]  # 움직일 수 있는 공간만큼 움직임
+                    board[i][j] = 0
+
+                if i - shift - 1 >= 0:  # 범위를 벗어 나지 않고
+                    if board[i - shift - 1][j] == board[i - shift][j] and not merged[i - shift - 1][j] and not \
+                    merged[i - shift][j]:  # 다 움직인 후에 위의 블록과 합칠 수 있으면 합침.
+                        board[i - shift - 1][j] *= 2
+                        board[i - shift][j] = 0
+                        merged[i - shift - 1][j] = True  # 연속적으로 합칠 수 없기 때문에 True로 만들어 줌
+
+    elif direction == 'DOWN':
+        for i in range(2, -1, -1):
+            for j in range(4):
+                shift = 0
+                for k in range(3 - i):
+                    if board[3 - k][j] == 0:  # 아래로 움직일 수 있는 공간이 있을 때
+                        shift += 1
+
+                if shift > 0:
+                    board[i + shift][j] = board[i][j]  # 움직일 수 있는 공간만큼 움직임
+                    board[i][j] = 0
+
+                if i + shift + 1 <= 3:  # 범위를 벗어 나지 않고
+                    if board[i + shift][j] == board[i + shift + 1][j] and not merged[i + shift + 1][j] and not \
+                    merged[i + shift][j]:  # 다 움직인 후에 위의 블록과 합칠 수 있으면 합침.
+                        board[i + shift + 1][j] *= 2
+                        board[i + shift][j] = 0
+                        merged[i + shift + 1][j] = True  # 연속적으로 합칠 수 없기 때문에 True로 만들어 줌
+
+    elif direction == 'LEFT':
+        for i in range(4):
+            for j in range(1, 4):
+                shift = 0
+                for k in range(j):
+                    if board[i][k] == 0:  # 왼쪽으로 움직일 수 있는 공간이 있을 때
+                        shift += 1
+
+                if shift > 0:
+                    board[i][j - shift] = board[i][j]  # 움직일 수 있는 공간만큼 움직임
+                    board[i][j] = 0
+
+                if j - shift - 1 >= 0:  # 범위를 벗어 나지 않고
+                    if board[i][j - shift] == board[i][j - shift - 1] and not merged[i][j - shift - 1] and not \
+                    merged[i][j - shift]:  # 다 움직인 후에 위의 블록과 합칠 수 있으면 합침.
+                        board[i][j - shift - 1] *= 2
+                        board[i][j - shift] = 0
+                        merged[i][j - shift - 1] = True  # 연속적으로 합칠 수 없기 때문에 True로 만들어 줌
+
+    elif direction == 'RIGHT':
+        for i in range(4):
+            for j in range(2, -1, -1):
+                shift = 0
+                for k in range(3 - j):
+                    if board[i][3 - k] == 0:  # 오른쪽으로 움직일 수 있는 공간이 있을 때
+                        shift += 1
+
+                if shift > 0:
+                    board[i][j + shift] = board[i][j]  # 움직일 수 있는 공간만큼 움직임
+                    board[i][j] = 0
+
+                if j + shift + 1 <= 3:  # 범위를 벗어 나지 않고
+                    if board[i][j + shift + 1] == board[i][j + shift] and not merged[i][j + shift + 1] and not \
+                    merged[i][j + shift]:  # 다 움직인 후에 위의 블록과 합칠 수 있으면 합침.
+                        board[i][j + shift + 1] *= 2
+                        board[i][j + shift] = 0
+                        merged[i][j + shift + 1] = True  # 연속적으로 합칠 수 없기 때문에 True로 만들어 줌
+
+    return board
 
 
 def create_home_button(screen, font, WHITE, BLACK):
